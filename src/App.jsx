ddd80bc3962d48a7ea5a090e2bdb4905b4096286
Jp Keys <jpmusik29@gmail.com>
2024-04-09 08:48:59 -0400

    Adding Routes using navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Topics from './components/Topics';
+import Practice from './components/Practice';
 
-function App() {
+export default function App() {
 
   return (
     <>
       <h1>Introduction to Statistics</h1> 
       <Routes>
-        
+        <Route index element={<Home />} />
+        <Route path='/topics' element={<Topics/>} />
+        <Route path='/practice' element={<Practice/>} />
       </Routes> 
     </>
   )
 }
 
-export default App
