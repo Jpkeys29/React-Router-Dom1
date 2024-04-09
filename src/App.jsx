7facf905426201bd68ead2ae756c07854a5c769c
Jp Keys <jpmusik29@gmail.com>
2024-04-09 08:01:48 -0400

    Setting up project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <h1>Introduction to Statistics</h1> 
+      <Routes>
+        
+      </Routes> 
+    </>
+  )
+}
+
+export default App
