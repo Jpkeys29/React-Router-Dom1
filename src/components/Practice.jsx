de368bc218bc6ed045f7e77923db868bf7239e9e
Jp Keys <jpmusik29@gmail.com>
2024-04-15 06:44:31 -0400

    Adding navbar

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Practice() {
     return(
-        <div>
-            <h1>It's time to put your knowledge in practice!</h1>
+        <div className="practice">
             <Link to={'/'}>Home</Link>
+            <h1>Practice</h1>
         </div>   
     )
 }
