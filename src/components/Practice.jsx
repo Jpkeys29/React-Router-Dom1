c5b093a4d18ef6a4b26c9b39e6f62fffbd501307
Jp Keys <jpmusik29@gmail.com>
2024-09-13 08:36:22 -0400

    Leaked fixed

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,28 +1,10 @@
 import { Link } from "react-router-dom";
-import { App as SendbirdApp } from '@sendbird/uikit-react';
-import '@sendbird/uikit-react/dist/index.css';
 
-function ChatComponent() {
-    return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            {/* <SendbirdApp
-            appId={"APPID"}
-            userId={"USERID"}
-             /> */}
+export default function Practice() {
+    return(
+        <div className="practice">
             <Link to={'/'}>Home</Link>
-
-        </div>
+            <h1>Practice</h1>
+        </div>   
     )
 }
-
-export default ChatComponent;
-
-
-// export default function Practice() {
-//     return(
-//         <div className="practice" style={{backgroundColor:'#ADD8E6', height:'100vh'}}  >
-//             <Link to={'/'}>Home</Link>
-//             <h1>Find a room</h1>
-//         </div>   
-//     )
-// }
