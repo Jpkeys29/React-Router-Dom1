a302979900a7d38f8490bab1b2075e4ae762fe7d
Jp Keys <jpmusik29@gmail.com>
2024-09-16 18:19:55 -0400

    Address autocomplete

---
+++ src/components/Resources.jsx
@@ -0,0 +1,50 @@
+import { GoogleMap, useJsApiLoader, StandaloneSearchBox } from '@react-google-maps/api';
+import { useRef } from 'react';
+
+const libraries = ["places"];
+
+function MyMap() {
+    const inputref = useRef(null);
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: import.meta.env.REACT_APP_GOOGLEMAPS_API_KEY,
+        libraries
+    })
+    console.log(isLoaded)
+
+    const handleOnPlacesChanged = () => {
+        let address = inputref.current.getPlaces()
+        console.log(address)
+    }
+
+    return (
+        <div style={{ marginTop: "10%", textAlign: 'center' }}>
+            {isLoaded &&
+                <StandaloneSearchBox
+                onLoad={(ref) => inputref.current = ref}
+                onPlacesChanged={handleOnPlacesChanged}
+                >
+                    <input
+                        type='tex'
+                        placeholder='Type address'
+                        style={{
+                            boxSizing: 'border-box',
+                            border: '1px solid transparent',
+                            width: '50%',
+                            height: '50px',
+                            padding: '0 12px',
+                            borderRadius: '3px',
+                            boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+                            fontSize: '14px',
+                            outline: 'none',
+                            textOverflow: 'ellipses',
+                            marginTop: '30px',
+                        }}
+                    />
+                </StandaloneSearchBox>
+            }
+        </div>
+    )
+}
+
+export default MyMap;
