22253826aaf850d4d4acb902d91417c3bb999106
Jp Keys <jpmusik29@gmail.com>
2024-09-20 07:26:08 -0400

    SearchResults component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Topics from './components/Topics';
+import GroupChat from './components/Topics';
 import ChatTrial from './components/Practice';
 import Welcome from './components/Welcome';
 import Resources from './components/Resources';
+import SearchResults from './components/SearchResults';
 import './index.css';
 import { Link, useParams} from 'react-router-dom';
 
 
 export default function App() {
-
   return (
     <div>
       <header className='header'>
-        <h1>Introduction to Statistics</h1>
-        <Link to={'/'}>Home</Link>
+        <h1>Hommye</h1>
+        <Link to={'/'}>Home</Link> 
       </header>
         <Routes>
           <Route index element={<Home />} />
-          <Route path='/topics' element={<Topics />} />
+          <Route path='/topics' element={<GroupChat />} />
           <Route path='/practice' element={<ChatTrial />} />
           <Route path='/resources' element={<Resources />} />
+          <Route path='/search' element={<SearchResults />} />
           <Route path='/hello/:fName/:lName' element={<Welcome />} />
         </Routes>
     </div>
