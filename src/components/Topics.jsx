8c94fbf753b5d7a4c3679ede32158dbe1b50d261
Jp Keys <jpmusik29@gmail.com>
2024-09-24 16:47:49 -0400

    Get Location API frontend

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,10 +1,37 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function Topics() {
-    return(
-        <div className='topics'>
-            <Link to={'/'}>Home</Link>
-            <h1>Topics</h1>
+const GetLocation = () => {
+    const [location, setLocation] = useState({postings: []})
+
+    useEffect(() => {
+        const fetchLocation = async () => {
+            try{
+                const response = await fetch("http://127.0.0.1:5000/api/get_posting");
+                if(!response.ok){
+                    throw new Error('Network response was not Ok')
+                }
+                const result = await response.json();
+                console.log(result)
+                setLocation(result);
+                // console.log(typeof location)
+            } catch(error) {
+                console.log('Error fetching area or neighborhood:', error);
+            }
+        };
+        fetchLocation();
+    }, []);
+    return (
+        <div>
+            <ul>
+                {location.postings && location.postings.map((l, index) => (
+                    <li key={l.id || index}>
+                        {typeof l.area === 'string' && <span>{l.area}</span>}
+                        {l.neighborhood}
+                    </li>
+                ))}
+            </ul>  
         </div>
     )
 }
+
+export default GetLocation;
